656c2c8bb03d31a6a1388edf036137b4c75e0088
manelp <manel.perez@intenthq.com>
2023-04-28 12:19:50 +0200

    Encode responses to json

--- app/src/main/kotlin/com/agilogy/timetracking/registration/infrastructure/TimeEntriesController.kt
+++ app/src/main/kotlin/com/agilogy/timetracking/registration/infrastructure/TimeEntriesController.kt
@@ -4,8 +4,9 @@ import arrow.core.Either
 import arrow.core.getOrElse
 import arrow.fx.coroutines.use
 import com.agilogy.db.hikari.HikariCp
+import com.agilogy.timetracking.project.domain.ProjectName
 import com.agilogy.timetracking.registration.domain.TimeEntriesRegister
-import com.agilogy.timetracking.registration.domain.TimeEntriesRepository
+import com.agilogy.timetracking.registration.domain.TimeEntry
 import com.agilogy.timetracking.user.domain.UserName
 import io.ktor.http.*
 import io.ktor.server.application.*
@@ -13,7 +14,16 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import jdk.jfr.Description
+import kotlinx.serialization.SerializationStrategy
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonArray
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.JsonPrimitive
+import java.time.Duration
+import java.time.Instant
 import java.time.LocalDate
+import java.time.temporal.ChronoUnit
 
 class TimeEntriesController(private val timeEntriesRegister: TimeEntriesRegister) {
 
@@ -21,16 +31,32 @@ class TimeEntriesController(private val timeEntriesRegister: TimeEntriesRegister
     fun timeEntriesRoutes() {
         // GET /time-entries/daily-user-hours?userName=userName&startDate=startDate&endDate=endDate
         get("/time-entries/daily-user-hours") {
-            val userName = call.request.queryParameters.parseRequiredParam("userName") { UserName(it) }
-            val startDate = call.request.queryParameters.parseRequiredParam("startDate") { LocalDate.parse(it) }
-            val endDate = call.request.queryParameters.parseRequiredParam("endDate") { LocalDate.parse(it) }
 
-            Either.zipOrAccumulate(userName, startDate, endDate) { u, s, d ->
-                val result = timeEntriesRegister.getDailyUserHours(u, s, d)
-                call.respondText(result.toString(), ContentType.Text.Plain)
+            Either.zipOrAccumulate(
+                call.request.queryParameters.parseRequiredParam("userName") { UserName(it) },
+                call.request.queryParameters.parseRequiredParam("startDate") { LocalDate.parse(it) },
+                call.request.queryParameters.parseRequiredParam("endDate") { LocalDate.parse(it) }
+            ) { userName, startDate, endDate ->
+                val result: List<Pair<LocalDate, Duration>> =
+                    timeEntriesRegister.getDailyUserHours(userName, startDate, endDate)
+                val jsonResult = JsonArray(result.map { (date, duration) ->
+                    JsonObject(
+                        mapOf(
+                            "date" to JsonPrimitive(date.toString()),
+                            "duration" to JsonPrimitive(duration.toString())
+                        )
+                    )
+                })
+                call.respondText(jsonResult.toString(), ContentType.Application.Json)
             }.getOrElse { errors ->
                 call.respondText(
-                    """{"message":${errors.joinToString(",", "[", "]")}}""",
+                    JsonObject(
+                        mapOf(
+                            "validationErrors" to JsonObject(
+                                errors.map { it.field to JsonPrimitive(it.description) }.toMap()
+                            )
+                        )
+                    ).toString(),
                     ContentType.Application.Json,
                     HttpStatusCode.BadRequest
                 )
@@ -39,18 +65,20 @@ class TimeEntriesController(private val timeEntriesRegister: TimeEntriesRegister
         }
     }
 
-    private fun <A> Parameters.parseRequiredParam(param: String, parser: (String) -> A): Either<String, A> =
+    private fun <A> Parameters.parseRequiredParam(param: String, parser: (String) -> A): Either<ValidationError, A> =
         this[param]?.let {
-            Either.catch { parser(it) }.mapLeft { "$param.invalid.format" }
-        } ?: Either.Left("$param.required")
+            Either.catch { parser(it) }.mapLeft { ValidationError(param, "invalid.format") }
+        } ?: Either.Left(ValidationError(param, "required"))
 
 
 }
 
+data class ValidationError(val field: String, val description: String)
+
 
 suspend fun main() {
 
-    HikariCp.dataSource("jdbc:postgresql://localhost/", "postgres", "postgres").use { dataSource ->
+    HikariCp.dataSource("jdbc:postgresql://localhost/test", "postgres", "postgres").use { dataSource ->
         val repo = PostgresTimeEntriesRepository(dataSource)
         val timeEntriesRegister = TimeEntriesRegister(repo)
 
